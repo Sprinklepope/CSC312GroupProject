f96724744bb4ba585234a9133f117ba65a2aa031
Ezra Fielding <ezra.fielding@gmail.com>
2020-09-07 18:33:24 +0200

    Created Base Views

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def flightselect(request):
+    return render(request, 'booking/flightselect.html')
+
+def seatselect(request):
+    return render(request, 'booking/seatselect.html')
+
+def infocollect(request):
+    return render(request, 'booking/infocollect.html')
+
+def payment(request):
+    return render(request, 'booking/payment.html')
+
+def confirmation(request):
+    return render(request, 'booking/confirmation.html')
