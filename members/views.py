6d4b067c873210c44756ad958dee8aab7ff67243
UlfhednarZA <mikhailvink11@gmail.com>
2020-09-08 21:21:27 +0200

    ./login, ./signup and ./homepage

--- members/views.py
+++ members/views.py
@@ -5,7 +5,6 @@ from .forms import RegisterForm
 
 
 def index(request):
-
     return render(request, "home/home.html")
 
 
@@ -13,7 +12,6 @@ def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
-
             form.save()
             return redirect("/")
 
