d101341978284c65fdbc61a0af62869757d4424d
Ezra Fielding <ezra.fielding@gmail.com>
2020-09-09 21:17:24 +0200

    Began Seat Selection Rendering

--- booking/urls.py
+++ booking/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('flight/', views.FlightSelectView.as_view(), name='booking-flight'),
-    path('seat/', views.seatselect, name='booking-seat'),
+    path('<flight>/seat/', views.seatselect, name='booking-seat'),
     path('info/', views.infocollect, name='booking-info'),
     path('pay/', views.payment, name='booking-pay'),
     path('confirm/', views.confirmation, name='booking-confirm'),
--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
+from django.urls import reverse
 from .models import Flight
 
 class FlightSelectView(ListView):
@@ -8,8 +10,24 @@ class FlightSelectView(ListView):
     context_object_name = 'flights'
     ordering = ['flightDateTime']
 
-def seatselect(request):
-    return render(request, 'booking/seatselect.html')
+@login_required
+def seatselect(request, flight):
+    if request.method == "POST":
+
+        if form.is_valid():
+            return redirect("/")
+    else:
+        flightInfo = Flight.objects.get(flightNo=flight)
+        col = int(flightInfo.planeID.seatsPerRow)
+        row = int(flightInfo.planeID.seatsAvailable / col)
+        mid = (col//2)
+    context = {
+        'flightInfo': flightInfo,
+        'row': range(row),
+        'col': range(col+1),
+        'mid': mid
+    }
+    return render(request, "booking/seatselect.html", context)
 
 def infocollect(request):
     return render(request, 'booking/infocollect.html')
